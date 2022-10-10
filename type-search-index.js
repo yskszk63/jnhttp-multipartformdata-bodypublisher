ff8085449bdb65990ae40e3d1e3c72366423b262
yskszk63 <yskszk63@users.noreply.github.com>
2022-10-10 13:45:41 +0000

    deploy: dc4e5dc27bf128e76153e31b8108810d8d7f8be9

--- type-search-index.js
+++ type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"com.github.yskszk63.jnhttpmultipartformdatabodypublisher","l":"MultipartFormDataBodyPublisher"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.github.yskszk63.jnhttpmultipartformdatabodypublisher","l":"MultipartFormDataBodyPublisher"}];updateSearchResults();
